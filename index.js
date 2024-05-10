aa131e67ae1e907ca8154cb3c311e3fbb3269355
YampX <juanpablodriz@gmail.com>
2024-05-09 21:36:13 -0300

    ...

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ app.post(URI, async (req, res) => {
 
 // API RX
 app.post("/api/rx", async (req, res) => {
-  const token = req.body.token;
+  const token = req.body.data.token;
   if (token !== TOKEN_AUTH) {
     return res.status(401).json({ error: "Unauthorized" });
   }
-  const text = req.body.data || "...";
+  const text = req.body.data.text || "...";
   await axios.post(`${TELEGRAM_API}/sendMessage`, {
     chat_id: ID_TELEGRAM,
     text: text,
